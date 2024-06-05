464dde9410e9491235ec90d45bbde96a3bc48f56
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-06-05 13:31:30 +0300

    new project

---
+++ NewsClient/NewsClient/MainTabbarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  NewsClient
+//
+//  Created by Aleksandr Moroshovskyi on 05.06.2024.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+}
